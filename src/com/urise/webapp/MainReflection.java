4e17b70cb4c9ab6639ff4c1d420d582c9224cd1f
chekmenev94 <andrei_94_chekm@mail.ru>
2023-04-29 20:21:09 +0300

    Исправил ошибки

--- src/com/urise/webapp/MainReflection.java
+++ src/com/urise/webapp/MainReflection.java
@@ -1,16 +1,10 @@
 package com.urise.webapp;
 
 import com.urise.webapp.model.Resume;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Arrays;
 
 public class MainReflection {
     public static void main(String[] args) throws IllegalAccessException, IOException, NoSuchMethodException, InvocationTargetException {
@@ -25,12 +19,5 @@ public class MainReflection {
         Method method = r.getClass().getMethod("toString");
         Object metod1 = method.invoke(r);
         System.out.println(metod1);
-
-
-
-
-
-
-
     }
 }
