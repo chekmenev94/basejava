f9c78570080a3adcbd3f76a8d2990adabb9decfb
chekmenev94 <andrei_94_chekm@mail.ru>
2023-05-29 21:32:38 +0300

    Lesson09 HW08

--- src/com/urise/webapp/storage/AbstractFileStorage.java
+++ src/com/urise/webapp/storage/AbstractFileStorage.java
@@ -3,8 +3,7 @@ package com.urise.webapp.storage;
 import com.urise.webapp.exception.StorageException;
 import com.urise.webapp.model.Resume;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -52,7 +51,7 @@ public abstract class AbstractFileStorage extends AbstractStorage<File> {
     @Override
     protected void doUpdate(File file, Resume r) {
         try {
-            doWrite(r, file);
+            doWrite(r, new BufferedOutputStream(new FileOutputStream(file)));
         } catch (IOException e) {
             throw new StorageException("Write file error", file.getName(), e);
         }
@@ -62,7 +61,7 @@ public abstract class AbstractFileStorage extends AbstractStorage<File> {
     protected void doSave(File file, Resume r) {
         try {
             file.createNewFile();
-            doWrite(r, file);
+            doWrite(r, new BufferedOutputStream(new FileOutputStream(file)));
         } catch (IOException e) {
             throw new StorageException("File save error", file.getName(), e);
         }
@@ -78,12 +77,11 @@ public abstract class AbstractFileStorage extends AbstractStorage<File> {
     @Override
     protected Resume doGet(File file) {
         try {
-            return doRead(file);
+            return doRead(new BufferedInputStream(new FileInputStream(file)));
         } catch (IOException e) {
             throw new StorageException("File read error", file.getName(), e);
         }
     }
-
 
     @Override
     public void clear() {
@@ -106,7 +104,6 @@ public abstract class AbstractFileStorage extends AbstractStorage<File> {
         return files.length;
     }
 
-    protected abstract Resume doRead(File file) throws IOException;
-
-    protected abstract void doWrite(Resume r, File file) throws IOException;
+    protected abstract Resume doRead(InputStream file) throws IOException;
+    protected abstract void doWrite(Resume r, OutputStream file) throws IOException;
 }
